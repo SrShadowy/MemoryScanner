5455310209d403ecd03117617e2d7a92eca4a55f
Guilherme Lima Araujo <38929104+apr3ndi5@users.noreply.github.com>
2019-03-25 23:10:02 -0300

    Update ScanMemory.hpp
    Tradução para inglês

--- FistScannn/FistScannn/ScanMemory.hpp
+++ FistScannn/FistScannn/ScanMemory.hpp
@@ -9,7 +9,7 @@ enum protectType : DWORD
 std::vector<MEMORY_BASIC_INFORMATION>mapp;
 
 bool mapmemory(HANDLE phandle, protectType pTopy) {//aprendiz saiu correndo, pau no cu de quem ta lendo!
-
+                                                   //The apprentice leaves running, dick in the ass of the one reading!
 	DWORD Proibido = (PAGE_NOCACHE | PAGE_GUARD | PAGE_NOACCESS);
 
 	MEMORY_BASIC_INFORMATION mbi;
